86bf12dcebcce72ecb9634a98a2db8c66a574d61
biswajit <ascebiswajit@gmail.com>
2024-05-30 05:07:56 +0530

    ..

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -5,6 +5,7 @@ const createUser = async (req:Request,res:Response,next:NextFunction) =>{
     const {name,email,password} = req.body;
 
         //validation
+        // if your application has big data u use express validator
     if(!name || !email || !password){
         const error = createHttpError(400,'All field are required');
         return next(error);
