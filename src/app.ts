a12bdbac4ffde445e3773ed6e2e77109d9c80dc1
biswajit <ascebiswajit@gmail.com>
2024-05-30 03:40:03 +0530

    Global Error Handler

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
-import express from 'express';
+import express from "express";
 
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 const app = express();
 
 //Routes
 //HTTPS Methods GET,POST,PUT,PATCH,DELETE
 
-app.get("/",(req,res,next)=>{
-    res.json({message:"THe app is running"})
-})
+app.get("/", (req, res, next) => {
+    //check that the error
+    // const error = createHttpError(400,'something wrong')
+    // throw error;
+    res.json({ message: "THe app is running" });
+});
+
+//global error handler
+app.use(globalErrorHandler)
 
 export default app;
