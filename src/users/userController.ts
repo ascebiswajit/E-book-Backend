9568d0d63405e871833ca13f681206bad21a8ad1
biswajit <ascebiswajit@gmail.com>
2024-05-31 17:47:31 +0530

    Add error handling to create user

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -2,8 +2,9 @@ import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
 import bcrypt from "bcrypt";
-import {sign} from "jsonwebtoken";
+import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import { User } from "./userTypes";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -15,30 +16,44 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(error);
   }
   //Database call
-  const user = await userModel.findOne({ email: email });
+  try {
+    const user = await userModel.findOne({ email: email });
 
-  if (user) {
-    const error = createHttpError(400, "User already exist with email");
-    return next(error);
+    if (user) {
+      const error = createHttpError(400, "User already exist with email");
+      return next(error);
+    }
+  } catch (error) {
+    return next(createHttpError(500, "Error while getting user"));
   }
 
-  //hash the password
-  const hashedPassword = await bcrypt.hash(password, 10);
+  let newUser: User;
+  try {
+    //hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-  //Process
-  const newUser = await userModel.create({
-    name,
-    email,
-    password: hashedPassword,
-  });
+    //Process
 
-  //Token generation
-  const token = sign({sub:newUser._id},config.jwtSecret as string,{expiresIn:"7d"})
+    newUser = await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+  } catch (error) {
+    return next(createHttpError(500, "Error while Creating user"));
+  }
+  try {
+    //Token generation
+    const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+    });
 
+    //Response
 
-  //Response
-
-  res.json({ accessToken:token });
+    res.json({ accessToken: token });
+  } catch (error) {
+    return next(createHttpError(500, "Doesnot getting JSON webtoken"));
+  }
 };
 
 export { createUser };
