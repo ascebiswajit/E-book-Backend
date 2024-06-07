a1fcd598c85181e1668c3a16b2b6586b7bd14d81
biswajit <ascebiswajit@gmail.com>
2024-06-08 05:20:17 +0530

    added the cors dynamic

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,33 @@ import userRouter from "./users/userRouter";
 import bookRouter from "./book/bookRouter";
 
 import cors from 'cors';
+import { config } from "./config/config";
 const app = express();
 app.use(express.json())
 
 // enable cors origin
-app.use(cors())
+
+// let whitelist = ['http://localhost:5173', 'http://localhost:3000']
+// app.use(cors({
+//     origin:whitelist,
+// }))
+
+//OR
+const whitelist = [config.eBook_Dashboard_Url, config.eBook_Client_Url];
+
+const corsOptions = {
+    origin: function (origin:any, callback:any) {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
+
+
+app.use(cors(corsOptions));
+
 
 //Routes
 //HTTPS Methods GET,POST,PUT,PATCH,DELETE
--- src/config/config.ts
+++ src/config/config.ts
@@ -8,5 +8,7 @@ const _config = {
     cloudinaryCloudNAme: process.env.CLOUDINARY_CLOUD_NAME, 
     cloudinaryApiKey: process.env.CLOUDINARY_API_KEY, 
     cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
+    eBook_Dashboard_Url: process.env.E_BOOK_DASHBOARD_LOCAL_URL,
+    eBook_Client_Url: process.env.E_BOOK_CLIENT_LOCAL_URL,
 };
 export const config = Object.freeze(_config);
